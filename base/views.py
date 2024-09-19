d989adf3bc6e1fe2386fb9087617d25053917981
fawad1997 <fawad_12@outlook.com>
2024-09-19 11:22:40 +0500

    Template set

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name':'Lets learn Python'},
+    {'id': 2, 'name':'Lets learn Java'},
+    {'id': 3, 'name':'Lets learn C Sharp'},
+    {'id': 4, 'name':'Lets learn Android'},
+]
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse("Room Page")
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if(i['id']==int(pk)):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
